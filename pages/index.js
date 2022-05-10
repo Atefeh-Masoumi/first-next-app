d9ceba85b6fa0b5f2a5cfa09e7153714856213b0
Atefeh-Masoumi <atefeh.masoomy@gmail.com>
2022-05-10 17:41:12 +0430

    hello dear error i missed you

--- pages/index.js
+++ pages/index.js
@@ -7,12 +7,10 @@ import Link from 'next/link'
 export default function Home() {
   return (
     <div>
-      <Navbar/>
+      
       <h1>Home page</h1>
       <Link href="/Atefeh"> listing</Link>
-      <Footer/>
-
-
+      
     </div>
   )
 }
