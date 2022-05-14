5f2b3c79c2a31bec062eeeb5b661b1c048bde54e
Atefeh-Masoumi <atefeh.masoomy@gmail.com>
2022-05-14 22:32:36 +0430

    feching products

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,17 @@
 
+import Head from 'next/head'
 import Link from 'next/link'
 export default function Home() {
   return (
+    <>
+    <Head>
+      <title>Next App</title>
+    </Head>
     <div>
-      
       <h1>Home page</h1>
       <Link href="/Atefeh"> Products</Link>
       
     </div>
+    </>
   )
 }
